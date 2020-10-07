d5d03d7951cfbb0ef4bb6bfad317e2ac042cc597
pwinzip <p.winzip@gmail.com>
2020-10-07 23:49:42 +0700

    read data from firestore

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebasedemo/screens/login.dart';
 import 'package:flutter/material.dart';
@@ -30,25 +31,109 @@ class _HomePageState extends State<HomePage> {
             )
           ],
         ),
-        body: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.network(
-                  '${user.photoURL}',
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                buildCard("Display Name: ${user.displayName}"),
-                buildCard("Email: ${user.email}"),
-                buildCard("Last Sign In: ${user.metadata.lastSignInTime}"),
-              ],
-            ),
-          ),
-        ),
+        body: realTimeRead("2"),
+        // oneTimeRead("2"),
+        // oneTimeFood(),
+        // realTimeFood(),
+        // Center(
+        //   child: SingleChildScrollView(
+        //     child: Column(
+        //       mainAxisAlignment: MainAxisAlignment.center,
+        //       children: [
+        //         oneTimeFood(),
+        //         // Image.network(
+        //         //   '${user.photoURL}',
+        //         // ),
+        //         // SizedBox(
+        //         //   height: 16,
+        //         // ),
+        //         // buildCard("Display Name: ${user.displayName}"),
+        //         // buildCard("Email: ${user.email}"),
+        //         // buildCard("Last Sign In: ${user.metadata.lastSignInTime}"),
+        //       ],
+        //     ),
+        //   ),
+        // ),
       ),
+    );
+  }
+
+  Widget realTimeFood() {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection("Foods").snapshots(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return CircularProgressIndicator();
+            break;
+          default:
+            return ListView(
+              children: makeListWidget(snapshot),
+            );
+        }
+      },
+    );
+  }
+
+  List<Widget> makeListWidget(AsyncSnapshot snapshot) {
+    return snapshot.data.docs.map<Widget>((document) {
+      return ListTile(
+        title: Text(document['food_name']),
+        subtitle: Text(document['price'].toString()),
+      );
+    }).toList();
+  }
+
+  Widget oneTimeFood() {
+    return FutureBuilder<QuerySnapshot>(
+      future: FirebaseFirestore.instance.collection("Foods").get(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return CircularProgressIndicator();
+            break;
+          default:
+            return ListView(
+              children: makeListWidget(snapshot),
+            );
+        }
+      },
+    );
+  }
+
+  oneTimeRead(String s) {
+    return FutureBuilder<DocumentSnapshot>(
+      future: FirebaseFirestore.instance.collection("Foods").doc(s).get(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return CircularProgressIndicator();
+            break;
+          default:
+            return ListTile(
+              title: Text(snapshot.data.data()['food_name']),
+              subtitle: Text(snapshot.data.data()['price'].toString()),
+            );
+        }
+      },
+    );
+  }
+
+  realTimeRead(String s) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection("Foods").doc(s).snapshots(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return CircularProgressIndicator();
+            break;
+          default:
+            return ListTile(
+              title: Text(snapshot.data.data()['food_name']),
+              subtitle: Text(snapshot.data.data()['price'].toString()),
+            );
+        }
+      },
     );
   }
 
